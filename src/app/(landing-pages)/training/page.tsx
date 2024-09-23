061cae4e90bddbd3c142e0d7f77f565a676eaaff
Anakor Joshua <becomingadev@gmail.com>
2024-09-23 15:13:30 +0100

    link training landing page to dashboard page

--- src/app/(landing-pages)/training/page.tsx
+++ src/app/(landing-pages)/training/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Training() {
   return (
     <header className="flex items-center justify-between gap-x-20 px-20 py-12">
       <Image src="/images/training.png" alt="" width={600} height={600} />
       <div className="max-w-[600px]">
-        <h1 className="text-5.5xl mb-8 font-bold leading-[3.5rem]">
+        <h1 className="mb-8 text-5.5xl font-bold leading-[3.5rem]">
           Welcome to Moricol Healthcare&apos;s Training Hub!
         </h1>
         <p>
@@ -20,9 +21,15 @@ export default function Training() {
           <button className="w-full rounded-lg bg-primary-500 px-9 py-3 text-primary-50">
             GET STARTED
           </button>
-          <button className="w-full rounded-lg border border-primary-500 px-9 py-3 text-primary-500">
+          {/* <button className="w-full rounded-lg border border-primary-500 px-9 py-3 text-primary-500">
             LOGIN
-          </button>
+          </button> */}
+          <Link
+            href="/dashboard/training"
+            className="inline-block w-full rounded-lg border border-primary-500 py-3 text-center text-primary-500 transition duration-300 hover:bg-primary-50"
+          >
+            LOGIN
+          </Link>
         </div>
       </div>
     </header>
