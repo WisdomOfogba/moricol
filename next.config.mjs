9669e638a97ba3448630bd3f2885aa29d3723324
Wisdom Ofogba <wisetega007@gmail.com>
2024-11-23 07:45:36 +0100

    removed ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreBuildErrors: true,
-  },
   images: {
     remotePatterns: [
       {
