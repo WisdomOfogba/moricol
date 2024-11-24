c614daa81bb5e2ffe47e15ec0f3cbc9a2a294bf6
Wisdom Ofogba <wisetega007@gmail.com>
2024-11-24 17:25:08 +0100

    fixed bug

--- src/app/dashboard/training/components/card2-course.tsx
+++ src/app/dashboard/training/components/card2-course.tsx
@@ -3,12 +3,21 @@ import { courseorder } from "@/definition";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function CourseCard2({courseData, type }: {courseData: courseorder, type: string} ) {
-  if(courseData.courseid?._id === undefined) {
+export default function CourseCard2({
+  courseData,
+  type,
+}: {
+  courseData: courseorder;
+  type: string;
+}) {
+  if (courseData.courseid?._id === undefined) {
     return;
   }
   return (
-    <Link href={`/dashboard/training/course/${type.toLowerCase()}/${courseData.courseid?._id}`} className="block">
+    <Link
+      href={`/dashboard/training/course/${type.toLowerCase()}/${courseData.courseid?._id}`}
+      className="block"
+    >
       <article className="border bg-white">
         {/* Image */}
         <div className="relative h-[11.7rem]">
@@ -26,16 +35,18 @@ export default function CourseCard2({courseData, type }: {courseData: courseorde
           <div className="mb-2.5 flex items-center justify-between">
             <span>
               <span className="bg-[#F9EBD0] px-1.5 py-1 text-[10px] font-medium text-[#714D0A]">
-              {type}
+                {type}
+              </span>
             </span>
-            
-                        </span>
-            <p className="text-xs text-[#E29A13]">{courseData.courseid?.level && `LEVEL ${courseData.courseid?.level}`}</p>
+            <p className="text-xs text-[#E29A13]">
+              {courseData.courseid?.level &&
+                `LEVEL ${courseData.courseid?.level}`}
+            </p>
           </div>
 
           {/* Title */}
           <h3 className="text-sm text-[#1D2026]">
-            {courseData.courseid?.title}
+            {courseData.courseid.title || courseData.courseid.bundle}
           </h3>
         </div>
 
