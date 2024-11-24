c09fc4c46ecc9833e8cbd525ee907744d8702aa4
codetective <iaminiodu@gmail.com>
2024-11-24 18:53:27 +0100

    FIX: redirect callback error

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,11 @@ import { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const originalUrl = req.nextUrl.protocol + req.headers.get('host') + req.nextUrl.pathname
-  console.log(req.url);
 
   if (req.nextUrl.pathname.startsWith("/dashboard")) {
     if (!token) {
       const signInUrl = new URL("/signin", originalUrl);
-      signInUrl.searchParams.set("callbackUrl", originalUrl);
+      signInUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
       return NextResponse.redirect(signInUrl);
     }
   }
