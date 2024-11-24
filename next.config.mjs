89f9505ad0bb81f7772d73da3af5fbeff4453c3d
Wisdom Ofogba <wisetega007@gmail.com>
2024-11-24 16:43:43 +0100

    added true

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds,
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors,
+    ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
